d9a1fdab74aff6016b254883d03328e619e8e496
Dmitri Gribanov <chychydo88@gmail.com>
2023-11-12 17:12:44 +0300

    Init commit

---
+++ src/main/java/com/javadml/growbox/GrowboxApplication.java
@@ -0,0 +1,13 @@
+package com.javadml.growbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javadml/growbox/GrowboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadml.growbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
